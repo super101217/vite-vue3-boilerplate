17ebaf11e16b88275cdc44b25ac02cc8472ea638
Superstar0107 <am000hw@gmail.com>
2021-07-09 23:21:59 -0300

    Adding alias resolvers

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
 import ViteComponents from 'vite-plugin-components'
+import * as path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [Vue(), Pages(), Layouts(), ViteComponents()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve('/src') },
+      { find: '~', replacement: path.resolve('/src') },
+    ],
+  },
 })
