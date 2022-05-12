9222d81ecba705046730602617d208998fee78b1
Superstar0107 <am000hw@gmail.com>
2022-05-12 01:05:17 -0300

    chore: updating readme, fixing ts warning

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { setupLayouts } from 'virtual:generated-layouts'
+// @ts-ignore - Generated pages doesn't have type configured
 import generatedRoutes from 'virtual:generated-pages'
 
 const routes = setupLayouts(generatedRoutes)
